c6f980ea9f93c4404fdc71a2f4e9beb82da09b77
Daniel Kafer <daniel.j.kafer@gmail.com>
2018-03-29 19:13:29 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <home-page />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container home-page">
+      <h1 class="header-text">
+          Oki Cospi
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style>
+    .header-text {
+        position: absolute;
+        font-size: 10em;
+        transform: translate(50%,50%);
+    }
+</style>
+
+
