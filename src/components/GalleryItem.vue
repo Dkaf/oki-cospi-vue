582fb34fd3bce4b35300b07b1bec4ecfd9cc2d99
Daniel Kafer <daniel.j.kafer@gmail.com>
2018-03-30 16:14:26 -0500

    Created gallery components

---
+++ src/components/GalleryItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container gallery-item">
+      <img :src="src" :alt="alt"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GalleryItem",
+  props: ['src', 'alt']
+}
+</script>
+
+<style lang="stylus" scoped>
+    img
+      max-width: 300px
+      max-height: auto
+</style>
+
---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container image-gallery">
+      <div v-for="photo in photos" v-bind:key="photo.id">
+          <gallery-item :src="photo.url" :alt="photo.alt" />
+      </div>
+  </div>
+</template>
+
+<script>
+import GalleryItem from './GalleryItem';
+
+export default {
+  name: 'ImageGallery',
+  components: {GalleryItem},
+  props: ['photos']
+}
+</script>
+
+<style lang="stylus" scoped>
+  .image-gallery
+    display: grid
+    grid-template-columns: repeat(3, 1fr)
+    grid-template-rows: minmax(100px, auto)
+    grid-gap: 10px
+    grid-column: 2
+</style>
+
+
