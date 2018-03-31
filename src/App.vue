9d29cf67d172c003c2d8324378b12bfc674e308b
Daniel Kafer <daniel.j.kafer@gmail.com>
2018-03-30 22:45:24 -0500

    Added coves font

--- src/App.vue
+++ src/App.vue
@@ -29,10 +29,25 @@ export default {
 </script>
 
 <style lang="stylus">
+
+@font-face {
+  font-family: 'Coves Light';
+  src: url("./assets/coves-font/coves_light-webfont.ttf") format("ttf"),
+       url("./assets/coves-font/coves_light-webfont.woff2") format("woff2"),
+       url("./assets/coves-font/coves_light-webfont.woff") format("woff")
+}
+
+@font-face {
+  font-family: 'Coves Bold';
+  src: url("./assets/coves-font/coves_bold-webfont.ttf") format("ttf"),
+       url("./assets/coves-font/coves_bold-webfont.woff2") format("woff2"),
+       url("./assets/coves-font/coves_bold-webfont.woff") format("woff")
+}
+
 body
   margin: 0;
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  #app
+    font-family: 'Coves Light', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
@@ -41,5 +56,6 @@ body
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: minmax(100px, auto);
     grid-gap: 10px;
-  }
+  .header-text
+    font-family: 'Coves Bold'
 </style>
