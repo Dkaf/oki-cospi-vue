f16ab6c24a179a3266d689108ba4611b4e594046
Dkaf <Daniel.j.kafer@gmail.com>
2018-04-20 21:39:09 -0500

    added fontawesome links to social media sites

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="footer-bar">
-
+    <a href="https://twitter.com/allyssooonnnn?ref_src=twsrc%5Etfw">
+      <font-awesome-icon class="icon twitter-icon" :icon="twitterIcon" size="2x" />
+    </a>
+    <a href="https://www.instagram.com/allyssooonnn/">
+      <font-awesome-icon class="icon instagram-icon" :icon="instagramIcon" size="2x" />
+    </a>
+    <a href="https://www.patreon.com/okicospi">
+      <font-awesome-icon class="icon patreon-icon" :icon="patreonIcon" size="2x" />
+    </a>
+    <a href="https://www.facebook.com/OkiCospi/">
+      <font-awesome-icon class="icon facebook-icon" :icon="facebookIcon" size="2x"/>
+    </a>
   </div>
 </template>
 
 <script>
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import {faTwitter, faInstagram, faFacebook, faPatreon} from '@fortawesome/fontawesome-free-brands'
+
+
 export default {
-  name: 'FooterBar'
+  name: 'FooterBar',
+  computed: {
+    twitterIcon() {
+      return faTwitter
+    },
+    instagramIcon() {
+      return faInstagram
+    },
+    facebookIcon() {
+      return faFacebook
+    },
+    patreonIcon() {
+      return faPatreon
+    }
+  },
+  components: {
+    FontAwesomeIcon
+  }
 }
 </script>
 
@@ -15,5 +47,8 @@ export default {
     background-color: #2c3e50
     grid-column: span 3
     height: 75px
+  .icon
+    color: white
+    padding: 10px
 </style>
 
