c5a876c20fecde4c84bffca7e91a33c7ad0ea1ce
Daniel Kafer <daniel.j.kafer@gmail.com>
2018-04-28 00:24:48 -0500

    start of blog post component

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="blog-post">
+    <h2 class="title">{{title}}</h2>
+    <span class="date">{{date}}</span>
+    <img :src="thumbnail" />
+    <p class="body">{{body}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogPost",
+  props: [
+    'title',
+    'date',
+    'body',
+    'thumbnail'
+  ]
+}
+</script>
