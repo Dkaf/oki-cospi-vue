9b9b713c03f981c3d276a09565c3db4a0452a471
Daniel Kafer <daniel.j.kafer@gmail.com>
2018-04-28 00:24:59 -0500

    start of patreon page component

---
+++ src/components/PatreonPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container patreon-page">
+    <span class="header-text">Patreon</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PatreonPage'
+}
+</script>
+
