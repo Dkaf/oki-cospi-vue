2aec30bb4931426ec9fc6f7772fdeaa7f1011d03
Daniel Kafer <daniel.j.kafer@gmail.com>
2018-05-17 11:58:57 -0500

    initial commit for current projects component

---
+++ src/components/CurrentProjects.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="current-projects">
+    <ul v-for="project in projects" :key="project.id">
+      <li>{{project.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import client from './../contentfulClient.js'
+
+export default {
+  name: 'CurrentProjects',
+  data: function() {
+    return {
+      projects: []
+    }
+  },
+  created: function() {
+    client.getEntries({'content_type':'currentProjects'})
+    .then(res => {
+      this.projects = res.items
+    }).catch(err => console.log(err))
+  }
+}
+</script>
+
