4b4234e849766ab1eb533e060743a499b3c2a936
Unknown <Daniel.j.kafer@gmail.com>
2018-06-20 13:53:59 -0500

    Added footer component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
     <nav-bar/>
-    <router-view/>
+    <router-view class="view"/>
+    <!-- <footer-bar /> -->
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar'
+import FooterBar from './components/FooterBar'
 
 export default {
   name: 'app',
   components: {
     NavBar,
+    FooterBar
   }
 }
 </script>
 
 <style lang="stylus">
 font-color = #2c3e50
+
+html, body
+  height: 100vh
 
 @font-face {
   font-family: 'Coves Light'
@@ -36,9 +42,24 @@ font-color = #2c3e50
 body
   margin: 0
   #app
+    @css {
+      grid-template-areas:
+        "nav "
+        "main";
+    }
+    display: grid
+    .view
+      grid-area: main
+    .nav-bar
+      grid-area: nav
     font-family: 'Coves Light', Helvetica, Arial, sans-serif
     -webkit-font-smoothing: antialiased
     -moz-osx-font-smoothing: grayscale
     text-align: center
     color: font-color
+    height: 100vh
+    h1, h2, h3
+      margin: 0
+      font-weight: normal
+      font-family: 'Coves Bold'
 </style>
