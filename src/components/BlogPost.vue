c71efa6076baa84d69b9069ebab37c16291f73ea
Unknown <Daniel.j.kafer@gmail.com>
2018-06-20 15:11:51 -0500

    Now gets and displays blog post content from cms

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,20 +1,48 @@
 <template>
-  <div class="blog-post">
-    <h2 class="title">{{title}}</h2>
-    <span class="date">{{date}}</span>
-    <img :src="thumbnail" />
-    <p class="body">{{body}}</p>
+  <div id="blog-post">
+    <h1>{{ post.data.title }}</h1>
+    <h4>{{ post.data.author.first_name }} {{ post.data.author.last_name }}</h4>
+    <div v-html="post.data.body"></div>
+
+    <router-link v-if="post.meta.previous_post" :to="/blog/ + post.meta.previous_post.slug" class="button">
+      {{ post.meta.previous_post.title }}
+    </router-link>
+    <router-link v-if="post.meta.next_post" :to="/blog/ + post.meta.next_post.slug" class="button">
+      {{ post.meta.next_post.title }}
+    </router-link>
   </div>
 </template>
 
 <script>
+import { butter } from '@/buttercms'
+
 export default {
-  name: "BlogPost",
-  props: [
-    'title',
-    'date',
-    'body',
-    'thumbnail'
-  ]
+  name: 'ButterPost',
+  data() {
+    return {
+      post: {}
+    }
+  },
+  methods: {
+    getPost() {
+      butter.post.retrieve(this.$route.params.slug)
+      .then(res => {
+        console.log(res.data)
+        this.post = res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.getPost()
+    }
+  },
+  created() {
+    this.getPost();
+  }
 }
 </script>
+
