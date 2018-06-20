bb9abaf36f2f113b5866f999c4177dd801bc4105
Unknown <Daniel.j.kafer@gmail.com>
2018-06-20 14:30:14 -0500

    Added box-shadow transition on hover

--- src/components/GalleryItem.vue
+++ src/components/GalleryItem.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="container gallery-item">
-      <img :src="src" :alt="alt"/>
+    <img class="gallery-img" @hover="imgHover" v-bind:style="{ background: overlay }" :src="src" :alt="alt" />
   </div>
 </template>
 
 <script>
-export default {
-  name: "GalleryItem",
-  props: ['src', 'alt']
-}
+  export default {
+    name: "GalleryItem",
+    props: ['src', 'alt']
+  }
 </script>
 
 <style lang="stylus" scoped>
   .gallery-item
     margin: 0
-    img
+    padding: 10px
+  .gallery-img
+    max-width: 70%
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1)
+    &:hover
+      box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)
+  @media screen and (min-width: 700px)
+    .gallery-img 
       max-width: 300px
-      max-height: auto
 </style>
-
