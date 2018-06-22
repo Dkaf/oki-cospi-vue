f6af8fd3031c863f75a6dc3f025a0910006f5fad
Dkaf <Daniel.j.kafer@gmail.com>
2018-06-21 19:26:04 -0500

    Now displays author info

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="blog-post">
+    <div class="author-info">
+      <img class="profile-img" :src="post.data.author.profile_image"/>
+      <h4 class="author">{{ post.data.author.first_name }} {{ post.data.author.last_name }}</h4>
+      <span class="bio">{{ post.data.author.bio }}</span>
+      <span class="date">{{ new Date(post.data.published).toDateString() }}</span>
+    </div>
     <h1 class="title">{{ post.data.title }}</h1>
-    <h4 class="author">{{ post.data.author.first_name }} {{ post.data.author.last_name }}</h4>
     <div class="body" v-html="post.data.body"></div>
 
     <router-link v-if="post.meta.previous_post" :to="/blog/ + post.meta.previous_post.slug" class="button">
@@ -17,7 +22,7 @@
 import { butter } from '@/buttercms'
 
 export default {
-  name: 'ButterPost',
+  name: 'BlogPost',
   data() {
     return {
       post: {}
@@ -49,13 +54,37 @@ export default {
 
 <style lang="stylus" scoped>
   .blog-post
-
+    display: flex
+    flex-flow: column
+    align-items: center
+    .author-info
+      display: grid
+      grid: 1fr 1fr 1fr / 1fr 1fr
+      font-size: 1.25em
+      text-align: left
+      align-items: center
+      .profile-img
+        grid-row: 1 / span 3
+        justify-self: center
+      .author
+        grid-area: 1 / 2
+        margin: 0
+      .bio
+        grid-area: 2 / 2
+      .date
+        grid-area: 3 / 2
+      .author
+      .bio
+      .date
+        opacity: .8
+        font-size: 1em
     .title
-      font-size: 10em
+      font-size: 6em
     .body
       font-size: 2em
       line-height: 1.58
       letter-spacing: -.003em
+      text-align: left
       width: 50%
       margin: 0 auto
       padding: 25px
