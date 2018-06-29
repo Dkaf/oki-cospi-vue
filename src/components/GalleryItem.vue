592d9cf255d3d1b5ebb0fd0efca6a01c52bba8d3
Dkaf <Daniel.j.kafer@gmail.com>
2018-06-28 23:13:34 -0500

    Changed images to be square

--- src/components/GalleryItem.vue
+++ src/components/GalleryItem.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="container gallery-item">
     <img class="gallery-img" @hover="imgHover" v-bind:style="{ background: overlay }" :src="src" :alt="alt" />
+    <img class="gallery-img" :src="src" :alt="alt" />
   </div>
 </template>
 
 <script>
   export default {
     name: "GalleryItem",
-    props: ['src', 'alt']
+    props: [
+      'src',
+      'alt'
+      ]
   }
 </script>
 
@@ -16,12 +20,13 @@
     margin: 0
     padding: 10px
   .gallery-img
-    max-width: 70%
     box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
     transition: all 0.3s cubic-bezier(.25,.8,.25,1)
     &:hover
       box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)
   @media screen and (min-width: 700px)
     .gallery-img 
-      max-width: 300px
+      height: 500px
+      width: 500px
+      object-fit: cover
 </style>
