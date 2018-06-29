9496dfee9a951c38b69a39128cd1295119a1b92c
Dkaf <Daniel.j.kafer@gmail.com>
2018-06-28 23:14:43 -0500

    Added product previews to store page

---
+++ src/components/ProductPreview.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="product-preview">
+    <router-link :to="'/store/' + slug">
+      <img class="product-img" :src="src" :alt="alt" />
+      <div class="product-info">
+        <span class="product-name">{{ name }}</span>
+        <span class="price">{{ price }}</span>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductPreview',
+  props:[
+    'src',
+    'alt',
+    'name',
+    'price',
+    'slug'
+  ]
+}
+</script>
+
+<style lang="stylus" scoped>
+  imgSize = 450px
+
+  .product-preview
+    display: flex
+    flex-flow: column
+  .product-img
+    height: imgSize
+    width: imgSize
+    object-fit: cover
+  .product-name
+    font-size: 1.5em
+</style>
--- src/components/StorePage.vue
+++ src/components/StorePage.vue
@@ -1,12 +1,46 @@
 <template>
   <div class="container store-page">
-    <span class="header-text">Store</span>
+    <h1 class="main-header">Store</h1>
+    <div class="products" v-for="(product, i) in products" :key="'product.slug' + '_' + i">
+      <product-preview :slug="product.slug" :src="images[i].link.href" :name="product.name" />
+    </div>
   </div>
 </template>
 
 <script>
+import { Moltin } from '@/moltin'
+import ProductPreview from './ProductPreview'
+
 export default {
-  name: 'StorePage'
+  name: 'StorePage',
+  components: {ProductPreview},
+  data() {
+    return {
+      products: [],
+      images: []
+    }
+  },
+  methods: {
+    getProducts() {
+      Moltin.Products.With('main_image').All().then(res => {
+        console.log(res)
+        this.products = res.data;
+        this.images = res.included.main_images
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  },
+  created() {
+    this.getProducts()
+  }
 }
 </script>
+
+<style lang="stylus" scoped>
+  .main-header
+    font-size: 5em
+</style>
+
 
