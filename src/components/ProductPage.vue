815f474bf33ff7cd6057e0e0be4a3065d6b7f7b9
Unknown <Daniel.j.kafer@gmail.com>
2018-07-11 01:39:53 -0500

    Initial commit of product page

---
+++ src/components/ProductPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product-page">
+    <h2 class="product-name">{{ selectedProduct.name }}</h2>
+    <img class="product-img" :src="selectedProduct.image">
+    <span class="price">{{ selectedProduct.price}}</span>
+    <!-- purchase button -->
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'ProductPage',
+  computed: {
+    ...mapGetters('shop',{
+      selectedProduct: 'viewProduct'
+    })
+  }
+}
+</script>
+
