3e5adeebdfd62d0d98d3d4ca1fc9985edd11c9a1
Unknown <Daniel.j.kafer@gmail.com>
2018-07-11 01:42:30 -0500

    Remove API calls from components
    Moved API calls and data to Vuex store

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,38 +1,23 @@
 <template>
   <div class="container image-gallery">
     <h1 data-aos="fade-right" class="header-text">Gallery</h1>
-      <gallery-item v-for="(image,i) in page.fields.gallery_images" :key="i" :src="image.image" alt="gallery image" />
+      <gallery-item v-for="(image,i) in images" :key="i" :src="image.image" alt="gallery image" />
   </div>
 </template>
 
 <script>
 import GalleryItem from './GalleryItem'
-import { butter } from '@/buttercms.js'
+import { mapState } from 'vuex';
 
 export default {
   name: 'ImageGallery',
   components: {GalleryItem},
   props: ['photos'],
-  data: function() {
-    return {
-      page: {
-        fields: {}
-      }
-    }
-  },
-  methods: {
-    getPage() {
-      butter.page.retrieve('*', 'image-gallery')
-      .then(res => {
-        this.page = res.data.data
-      })
-      .else(err => {
-        console.log(err)
-      })
-    }
-  },
-  created: function() {
-    this.getPage()
+  computed: mapState({
+      images: state => state.gallery.galleryImages
+  }),
+  created() {
+    this.$store.dispatch('gallery/getPage')
   }
 }
 </script>
