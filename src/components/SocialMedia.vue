3a3712742a9329f6cd2728a602f57914c1defa57
Unknown <Daniel.j.kafer@gmail.com>
2018-07-11 01:44:25 -0500

    Temporarily commented out twitter widget

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -2,7 +2,7 @@
   <div class="container social-media">
     <!-- <transition-title text="Social" /> -->
     <h1 data-aos="fade-right" class="header-text main-header">Social</h1>
-    <div id="twitter-container">
+    <!-- <div id="twitter-container">
       <h2 data-aos="fade-up" class="header-text sub-header">Twitter</h2>
       <a class="twitter-timeline"
          data-chrome="transparent nofooter noborders"
@@ -10,11 +10,11 @@
          href="https://twitter.com/allyssooonnnn?ref_src=twsrc%5Etfw">
         Tweets by allyssooonnnn
       </a>
-    </div>
+    </div> -->
     <div id="instagram-container">
       <h2 data-aos="fade-up" class="header-text sub-header">Instagram</h2>
       <!-- LightWidget WIDGET -->
-      <iframe src="//lightwidget.com/widgets/93ab91e65f3a53838d541a449db8c9df.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+      <iframe src="//lightwidget.com/widgets/93ab91e65f3a53838d541a449db8c9df.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 80%; border: 0; overflow: hidden;"></iframe>
     </div>
   </div>
 </template>
@@ -35,8 +35,7 @@ export default {
     font-size: 3em
   .main-header
     flex-basis: 100%
-  #twitter-container
   #instagram-container
-    max-width: 80%
+    width: 100%
 </style>
 
