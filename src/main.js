b4383a919f81eca2be4481c9f23c762ad3f487a0
Unknown <Daniel.j.kafer@gmail.com>
2018-07-11 01:39:16 -0500

    Added vuex-router-sync

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { sync } from 'vuex-router-sync'
 
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import store from './store'
+import store from './store/index'
 
 Vue.config.productionTip = false
+
+sync(store, router)
 
 new Vue({
   created() {
     AOS.init()
   },
-
   router,
   store,
   render: h => h(App)
