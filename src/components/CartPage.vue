a5d9cb3d258825053a982db8d821f0cac67feed4
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-23 19:16:50 -0500

    Initial cart page

---
+++ src/components/CartPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cart-page">
+    <div class="cart-contents">
+      <!-- <cart-item v-for="item in items" 
+        :key="item.id"
+        :img="item.image.href"
+        :name="item.name"
+        :quantity="item.quantity"
+        :id="item.id"
+        :price="item.meta.display_price.without_tax.value.formatted"
+        :description="item.description"
+      /> -->
+    </div>
+    <router-link to="/store/checkout">
+      <button>Checkout</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import CartItem from './CartItem'
+
+export default {
+  name: 'CartPage',
+  components: { CartItem },
+  computed: mapState({
+    cart: state => state.shop.cart.items
+  })
+}
+</script>
+
