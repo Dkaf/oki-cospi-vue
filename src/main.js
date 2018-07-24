353d2ec7fa8284ea56e8ce9b721c4401c6b7e19c
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-24 13:20:09 -0500

    Add imports for font awesome, prismic, vuex store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+//Vue Router
 import router from './router'
 import { sync } from 'vuex-router-sync'
+import store from './store/index'
 
+//Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCartPlus, faArrowCircleLeft, faPlus, faMinus, faTimes, faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCartPlus, faShoppingCart, faArrowCircleLeft, faPlus, faMinus, faTimes)
+
+//Animate CSS
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import store from './store/index'
+
+	
+import PrismicVue from 'prismic-vue';
+import linkResolver from './link-resolver';
+	
+Vue.use(PrismicVue, {
+  endpoint: window.prismic.endpoint,
+  linkResolver
+});
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
