561ebf903032ecfb12c5c28dc96d4a9c0621e9f0
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-23 19:17:13 -0500

    Initial store checkout page

---
+++ src/components/CheckoutPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="checkout-page">
+    <payment-form />
+  </div>  
+</template>
+
+<script>
+import PaymentForm from './PaymentForm'
+
+export default {
+  name:'CheckoutPage',
+  components: { PaymentForm }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .checkout-page
+    display: flex
+    flex-flow: column
+    align-items: center
+</style>
+
+
+
