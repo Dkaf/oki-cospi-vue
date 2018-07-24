aa31a708afcda939f19b28b14a671f35af17c896
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-23 19:16:36 -0500

    Initial cart item component

---
+++ src/components/CartItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="cart-item">
+    <router-link :to="'/store/' + slug">
+      <img class="thumbnail" :src="img" :alt="alt" />
+      <div class="details">
+        <span class="name">{{ name }}</span>
+        <span class="description">{{ description }}</span>
+        <span class="quantity">{{ quantity.toString() }}</span>
+      </div>
+    </router-link>
+    <font-awesome-icon icon="times" class="remove-icon" @click="removeItem(id, quantity)" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartItem',
+  props: {
+    img: String,
+    alt: String,
+    name: String,
+    description: String,
+    quantity: Number,
+    id: String,
+    slug: String,
+    price: String
+  },
+  methods: {
+    removeItem(id, quantity) {
+      this.$store.dispatch('shop/removeFromCart', {id, quantity})
+    }
+  }
+}
+</script>
+
