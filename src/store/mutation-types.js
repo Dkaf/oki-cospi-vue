28844aca6372954cf25900920640881c4aae79b8
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-23 21:06:17 -0500

    Added cart mutation types

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,6 +1,10 @@
 //shop module mutation types
 export const shopMutations = {
-  SET_PRODUCTS: 'SET_PRODUCTS'
+  SET_PRODUCTS: 'SET_PRODUCTS',
+  UPDATE_CART: 'UPDATE_CART',
+  CREATE_CART: 'CREATE_CART',
+  GET_CART: 'GET_CART',
+  DELETE_CART: 'DELETE_CART'
 }
 
 //blog module mutation types
