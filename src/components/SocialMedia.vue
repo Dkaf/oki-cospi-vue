03ba61bb69867ca18026377abff3abaf06417144
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-23 19:29:44 -0500

    changed animation duration and added anchors

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container social-media">
     <!-- <transition-title text="Social" /> -->
-    <h1 data-aos="fade-right" class="header-text main-header">Social</h1>
+    <h1 data-aos="fade-right" data-aos-duration="1500" data-aos-anchor=".lightwidget-widget" class="header-text main-header">Social</h1>
     <!-- <div id="twitter-container">
       <h2 data-aos="fade-up" class="header-text sub-header">Twitter</h2>
       <a class="twitter-timeline"
@@ -12,7 +12,7 @@
       </a>
     </div> -->
     <div id="instagram-container">
-      <h2 data-aos="fade-up" class="header-text sub-header">Instagram</h2>
+      <h2 data-aos="fade-up" data-aos-duration="1500" data-aos-anchor=".lightwidget-widget" class="header-text sub-header">Instagram</h2>
       <!-- LightWidget WIDGET -->
       <iframe src="//lightwidget.com/widgets/93ab91e65f3a53838d541a449db8c9df.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 80%; border: 0; overflow: hidden;"></iframe>
     </div>
