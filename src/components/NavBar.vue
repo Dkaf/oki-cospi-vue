a3e5aad509ee16f1da9130af124124f087c1bc60
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-23 19:28:39 -0500

    Added dynamic page title and router link to shopping cart

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,13 +3,19 @@
     <div class="logo-container">
       <img id="logo" :class="{tada: isAnimated}" class="animated" src="./../assets/Sticker.png" />
     </div>
-    <router-link v-for="(link, index) in links"
-      :key="index"
-      :to="link.path"
-      :class="linkColor"
-    >
-      {{link.name}}
+    <h1 class="page-title">{{pageTitle}}</h1>
+    <div class="link-container">
+      <router-link v-for="(link, index) in links"
+        :key="index"
+        :to="link.path"
+        class="white"
+      >
+        {{link.name}}
+      </router-link>
+    <router-link to="/store/cart" class="cart-link">
+      <font-awesome-icon icon="shopping-cart" class="cart-icon" size="sm" style="color: white;"/>
     </router-link>
+    </div>
   </div>
 </template>
 
@@ -24,7 +30,8 @@ export default {
         {path: '/store', name: 'Store'}
       ],
       linkColor: 'white',
-      isAnimated: false
+      isAnimated: false,
+      currentPath: this.$route.path 
     }
   },
   methods: {
@@ -34,21 +41,42 @@ export default {
   },
   created: function() {
     setInterval(this.triggerAnimate, 3000)
-    this.$route.path !== '/' ? this.linkColor = 'grey' : this.linkColor = 'white'
+    // this.$route.path !== '/' ? this.linkColor = 'grey' : this.linkColor = 'white'
   },
-  watch: {
-    '$route' () {
-      this.$route.path !== '/' ? this.linkColor = 'grey' : this.linkColor = 'white'
+  computed: {
+    pageTitle() {
+      if(this.$route.path == '/blog') {
+        return 'A Very Oki Blog'
+      }
+      else if(this.$route.path == '/store') {
+        return 'Store'
+      } else {
+        return 'Oki Cospi'
+      }
     }
   }
+  // watch: {
+  //   '$route' () {
+  //     this.$route.path !== '/' ? this.linkColor = 'grey' : this.linkColor = 'white'
+  //   }
+  // }
 }
 </script>
 
 <style lang="stylus" scoped>
   font-color = #2c3e50
+
+  .page-title
+    color: white
+    font-size: 5em
+    align-self: flex-end
   .nav-bar
     display: flex
     flex-flow: column
+    background: url('./../assets/blog-header.gif') no-repeat
+    background-position: center bottom
+    background-color: #ca485a
+    grid-area: nav
     a
       font-size: 2.5em
       text-decoration: none
@@ -56,7 +84,6 @@ export default {
     #logo
       height: 100px
       padding: 25px
-      position: absolute
   .grey
     color: font-color
     &:active
@@ -68,9 +95,13 @@ export default {
   @media screen and (min-width: 700px)
     .nav-bar
       flex-flow: row
-      justify-content: flex-end
+      justify-content: space-between
       #logo
-        height: 200px
-      .logo-container
-        margin-right: auto
+        height: 125px
+      .link-container
+        display: flex
+      .cart-link
+        align-self: flex-end
+        .cart-icon
+          padding: 15px
 </style>
