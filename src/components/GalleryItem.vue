d2439541c2ceee87d5606ce5610d4b6ef49a6177
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-26 00:03:48 -0500

    added active toggle on click

--- src/components/GalleryItem.vue
+++ src/components/GalleryItem.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container gallery-item">
-    <img class="gallery-img" @hover="imgHover" v-bind:style="{ background: overlay }" :src="src" :alt="alt" />
-    <!-- <img class="gallery-img" :src="src" :alt="alt" /> -->
+    <transition name="zoom">
+      <img class="gallery-img" :style="{ background: overlay }" :src="src" :alt="alt" @click="toggleActive" :active="isActive" />
+    </transition>
   </div>
 </template>
 
@@ -13,7 +14,17 @@
       'alt',
       'imgHover',
       'overlay'
-      ]
+    ],
+    data() {
+      return {
+        isActive: false
+      }
+    },
+    methods: {
+      toggleActive: () => {
+        return this.isActive = !this.isActive
+      }
+    }
   }
 </script>
 
@@ -22,10 +33,16 @@
     margin: 0
     padding: 10px
   .gallery-img
+    cursor: pointer
     box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
+    filter: brightness(90%)
     transition: all 0.3s cubic-bezier(.25,.8,.25,1)
     &:hover
       box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)
+      filter: brightness(100%)
+  .zoom-enter-active
+    transition: all 0.3s ease
+    transform: translateScale(1.5)
   @media screen and (min-width: 700px)
     .gallery-img 
       height: 500px
