783a7f67148f11e0f177bdbdef6e86b212ad1b1b
Dkaf <Daniel.j.kafer@gmail.com>
2018-07-26 00:02:08 -0500

    added cart actions and mutations

--- src/store/modules/shop.js
+++ src/store/modules/shop.js
@@ -1,9 +1,10 @@
 import { Moltin } from '@/moltin'
 import { shopMutations } from '../mutation-types'
-import router from '@/router'
 
 const state = {
-  products: []
+  products: [],
+  cart: {},
+  notification: ''
 }
 
 const getters = {
@@ -21,7 +22,37 @@ const actions = {
       commit(shopMutations.SET_PRODUCTS, {products: res.data, images: res.included.main_images})
     })
     .catch(err => {
-      console.log(err)
+      throw new Error(err.message)
+    })
+  },
+  getCart({ commit, state }) {
+    Moltin.Cart(state.cart.id).Items()
+    .then(res => {
+      commit(shopMutations.UPDATE_CART, res)
+    })
+  },
+  addToCart({ commit, state, dispatch }, {id, amount}) {
+    if(state.cart === {}) {
+      const cart = Moltin.Cart()
+      commit(shopMutations.CREATE_CART, cart)
+    }
+    Moltin.Cart(state.cart.id).AddProduct(id, amount).then(() => {
+      return dispatch('getCart')
+    })
+    .catch(err => {
+      throw new Error(err.message)
+    })
+  },
+  removeFromCart({ commit, state, dispatch }, {id, amount}) {
+    Moltin.Cart(state.cart.id).RemoveItem(id, amount).then(() => {
+      return dispatch('getCart')
+    })
+  },
+  deleteCart({ commit, state }) {
+    Moltin.Cart(state.cart.id).Delete()
+    .then(res => {
+      console.log(res)
+      commit(shopMutations.DELETE_CART)
     })
   }
 }
@@ -32,13 +63,24 @@ const mutations = {
     state.products = products.map((product,i) => {
       return {
         name: product.name,
+        description: product.description,
         price: product.meta.display_price.without_tax.formatted,
         quantity: product.meta.stock.level,
         slug: product.slug,
-        image: images[i].link.href,
+        image: images[i] ? images[i].link.href : '#',
         id: product.id
       }
     })
+  },
+  [shopMutations.CREATE_CART](state, cart) {
+    state.cart = cart
+  },
+  [shopMutations.UPDATE_CART](state, items) {
+    state.cart.items = items
+  },
+  [shopMutations.DELETE_CART](state) {
+    state.notification = "Cart deleted"
+    state.cart = {}
   }
 }
 
